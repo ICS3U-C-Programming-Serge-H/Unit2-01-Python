b9d48e61e1b78a5c366dae16889d612e47d15c83
Serge Hamouche <Serge.el-hamouche@stu.ocsb.ca>
2025-02-25 18:07:29 +0000

    commit area_per_rect_input.py

---
+++ “area_per_rect_input.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Created By: Serge Hamouche
+# Date:Feb 23, 2025
+# This program displays Hello, World! with proper style.
+
+length = int(input("Enter length of the rectangle (25): "))
